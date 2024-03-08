94452669cc5f5ebe8fb6f60c9c097c13a2d20461
Sanjula-Dulshan <sdulshan10@gmail.com>
2024-03-08 13:47:12 +0530

    get values using template reference variables

--- src/app/my-test/child/child.component.ts
+++ src/app/my-test/child/child.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { DataService } from '../data.service';
 
 @Component({
@@ -11,16 +18,13 @@ export class ChildComponent {
 
   @Input() message: string = '';
   @Output() messageEvent = new EventEmitter<string>();
-  childMessage: string = '';
+
   messageFromSibling: string = '';
-  counter: number = 1;
 
   childMessage2: string = 'From child component 2!'; //View Child
 
-  sendMessageToParent() {
-    this.childMessage = `Message from child ${this.counter}`;
-    this.messageEvent.emit(this.childMessage);
-    this.counter++;
+  sendMessageToParent(value: string) {
+    this.messageEvent.emit(value);
   }
 
   ngOnInit() {
